669b2effeecb9288065875460ffb6ee657dfa6e1
bishmie <bishmimalshi@gmail.com>
2024-11-04 18:34:29 +0530

    first commit

---
+++ src/main/java/lk/ijse/controller/LoginPageController.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginPageController {
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserId;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegesterOnAction(ActionEvent event) {
+
+    }
+
+}
